34053fa427aa4acf0856664156b27be5b6e88651
Dmitry Matrokhin <matrokhin.d@gmail.com>
2020-06-28 20:24:01 +0300

    Avoid rows copying

--- src/view/view.cpp
+++ src/view/view.cpp
@@ -1,15 +1,13 @@
 #include "view.h"
 
-View::View(uint32_t w, uint32_t h)
-    : width(w), height(h), pixels(w * h) {}
+View::View(uint32_t w, uint32_t h) : width(w), height(h), pixels(h) {}
 
-Color& View::operator()(uint32_t x, uint32_t y) { return pixels[y * width + x]; }
+Color& View::operator()(uint32_t x, uint32_t y) { return pixels[y][x]; }
 const Color& View::operator()(uint32_t x, uint32_t y) const {
-  return pixels[y * width + x];
+  return pixels[y][x];
 }
 
-void View::apply_row(const ViewRow& i_row, uint32_t y) {
-  // TODO_performance: Should we store vector of vectors instead and avoid copying?
-  std::copy(i_row.begin(), i_row.end(), pixels.begin() + y * width);
+void View::apply_row(ViewRow&& i_row, uint32_t y) {
+  pixels[y] = std::move(i_row);
 }
 
--- src/view/view.h
+++ src/view/view.h
@@ -10,10 +10,10 @@ using ViewRow = std::vector<Color>;
 struct View {
   uint32_t width = 0;
   uint32_t height = 0;
-  std::vector<Color> pixels;
+  std::vector<std::vector<Color>> pixels;
 
   View(uint32_t w, uint32_t h);
   Color& operator()(uint32_t x, uint32_t y);
   const Color& operator()(uint32_t x, uint32_t y) const;
-  void apply_row(const ViewRow& i_row, uint32_t y);
+  void apply_row(ViewRow&& i_row, uint32_t y);
 };
